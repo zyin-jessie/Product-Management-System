67b50ef39946a19641ef50f1f6e378147848c235
Jessie Bartolome <macjessie.bartolome@gmail.com>
2025-03-14 22:36:32 +0800

    Added login function

--- frontend/src/app/(auth)/auth/components/loginForm.tsx
+++ frontend/src/app/(auth)/auth/components/loginForm.tsx
@@ -2,8 +2,23 @@
 import React from 'react'
 import Link from 'next/link'
 import toast from 'react-hot-toast'
+import { useFormik } from 'formik'
 
+interface values {
+    email: string,
+    password: string
+}
 const LoginForm = () => {
+
+    const { values, handleSubmit, handleChange, handleBlur } = useFormik({
+        initialValues: {
+        email: "",
+        password: ""
+    },
+    onSubmit: async (values: values) => {
+        console.log(values);
+    }
+});
   return (
     <div className='h-screen flex items-center justify-center'>
         <div className='shadow-lg p-5 py-10 rounded-md'>
@@ -12,13 +27,14 @@ const LoginForm = () => {
                 <p>Don&rsquo;t have an account? <Link href="/auth/signup">Signup</Link></p>
             </div>
 
-            <form action="#" method="POST">
+            <form method="POST" onSubmit={handleSubmit}>
                 <div className='flex flex-col gap-y-2'>
-                    <input type="text" placeholder='Email' className='text-field' />
-                    <input type="text" placeholder='Password' className='text-field' />
+                    <input type="text" placeholder='Email' className='text-field' name='email'
+                    value={values.email} onChange={handleChange} onBlur={handleBlur} />
+                    <input type="text" placeholder='Password' className='text-field' name='password'
+                    value={values.password} onChange={handleChange} onBlur={handleBlur} />
                 </div>
-
-                <Link className='submit-btn mt-2' href="#" onClick={() => toast.success("Logged in!")}>Login</Link>
+                <button type='submit' className='submit-btn mt-2' onClick={() => toast.success("Logged in!")}>Login</button>
 
             </form>
         </div>

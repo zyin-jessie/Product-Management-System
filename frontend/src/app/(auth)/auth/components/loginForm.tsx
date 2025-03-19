854872d680711698d565eedb003a78f69156fe1a
Jessie Bartolome <macjessie.bartolome@gmail.com>
2025-03-19 18:36:27 +0800

    Modify Login & Signup UI

--- frontend/src/app/(auth)/auth/components/loginForm.tsx
+++ frontend/src/app/(auth)/auth/components/loginForm.tsx
@@ -29,74 +29,73 @@ const LoginForm = () => {
             router.push("/dashboard")
         }, 3000);
     }
-});
+    });
   return (
-<section className="bg-white lg:grid lg:min-h-screen lg:grid-cols-12">
-    <section className="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
+    <section className="bg-white lg:grid lg:min-h-screen lg:grid-cols-12">
+        <section className="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
 
-      <Image alt="" src="/gas-station.png" width={1000} height={1000} className="absolute inset-0 h-full w-full object-cover "/>
+        <Image alt="" src="/gas-station.png" width={1000} height={1000} className="absolute inset-0 h-full w-full object-cover "/>
 
-      <div className="hidden lg:relative lg:block lg:p-12">
-        <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
-            Product Management System
-        </h2>
+        <div className="hidden lg:relative lg:block lg:p-12">
+            <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
+                Product Management System
+            </h2>
 
-        <p className="mt-4 leading-relaxed text-white/90">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem reprehenderit nemo qui excepturi, facilis aliquid enim suscipit necessitatibus ipsam cumque quia debitis illo nulla praesentium doloremque iusto accusantium in vero.
-        </p>
-      </div>
-    </section>
-
-    <main className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6">
-      <div className="max-w-xl lg:max-w-3xl">
-        <div className="relative -mt-16 block lg:hidden">
-          <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
-            Product Management System
-          </h1>
-
-          <p className="mt-4 leading-relaxed text-gray-500">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
-            quibusdam aperiam voluptatum.
-          </p>
+            <p className="mt-4 leading-relaxed text-white/90">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem reprehenderit nemo qui excepturi, facilis aliquid enim suscipit necessitatibus ipsam cumque quia debitis illo nulla praesentium doloremque iusto accusantium in vero.
+            </p>
         </div>
+        </section>
 
-        <form method='POST' onSubmit={handleSubmit} className="flex flex-col gap-4 lg:px-20">
+        <main className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6">
+        <div className="max-w-xl lg:max-w-3xl">
+            <div className="relative -mt-16 block lg:hidden">
+            <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
+                Product Management System
+            </h1>
 
-            <div className='flex flex-col gap-y-1 mb-5'>
-                <p className='text-3xl font-semibold text-[#333]'>Sign In</p>
-                <p className='text-gray-500 text-sm'>Don&rsquo;t have an account? <Link href="/auth/signup" className='text-gray-800'>Signup</Link></p>
+            <p className="mt-4 leading-relaxed text-gray-500">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum architecto repellat, aliquid laudantium illo quo praesentium ex quam vel exercitationem velit delectus quis id harum! Inventore praesentium voluptatem corporis fugiat!
+            </p>
             </div>
 
-            <div className='flex flex-col gap-y-3'>
-                <div>
-                    <input type="text" id="FirstName" value={values.email} onChange={handleChange} onBlur={handleBlur}  placeholder='Email' name="email" className={`text-field ${errors.email && touched.email ? "border-2 border-danger focus:outline-none" : ""}`} />
-                    {errors.email && touched.email && <p className='error-text'>{errors.email}</p>}
+            <form method='POST' onSubmit={handleSubmit} className="flex flex-col gap-4 w-md">
+
+                <div className='flex flex-col gap-y-1 mb-5'>
+                    <p className='text-3xl font-semibold text-[#333]'>Sign In</p>
+                    <p className='text-gray-500 text-sm'>Don&rsquo;t have an account? <Link href="/auth/signup" className='text-gray-800'>Signup</Link></p>
                 </div>
-                <div>
-                    <input type="password" id="FirstName" value={values.password} onChange={handleChange} onBlur={handleBlur} placeholder='Password' name="password" className={`text-field ${errors.password && touched.password ? "border-2 border-danger focus:outline-none" : ""}`}/>
-                    {errors.password && touched.password && <p className='error-text'>{errors.password}</p>}
+
+                <div className='flex flex-col gap-y-3'>
+                    <div>
+                        <input type="text" id="FirstName" value={values.email} onChange={handleChange} onBlur={handleBlur}  placeholder='Email' name="email" className={`text-field ${errors.email && touched.email ? "border-2 border-danger focus:outline-none" : ""}`} />
+                        {errors.email && touched.email && <p className='error-text'>{errors.email}</p>}
+                    </div>
+                    <div>
+                        <input type="password" id="FirstName" value={values.password} onChange={handleChange} onBlur={handleBlur} placeholder='Password' name="password" className={`text-field ${errors.password && touched.password ? "border-2 border-danger focus:outline-none" : ""}`}/>
+                        {errors.password && touched.password && <p className='error-text'>{errors.password}</p>}
+                    </div>
                 </div>
-            </div>
 
-            <div className="hover:text-blue-600 text-right hover:underline underline-offset-4 transition-all text-sm">Forgot password?</div>
+                <div className="hover:text-blue-600 text-right hover:underline underline-offset-4 transition-all text-sm">Forgot password?</div>
 
-            <div className="col-span-6 text-sm">
-                <span>By using this service, you understood and agree to the ClickCart Online Services </span>
-                <Link href="#" className='text-blue-600'>Terms of Use</Link>
-                <span className="mx-[5px]">and</span>
-                <a href="#" className="text-blue-600">Privacy Statement</a>
-                <span>.</span>
-            </div>
+                <div className="col-span-6 text-sm">
+                    <span>By using this service, you understood and agree to the ClickCart Online Services </span>
+                    <Link href="#" className='text-blue-600'>Terms of Use</Link>
+                    <span className="mx-[5px]">and</span>
+                    <a href="#" className="text-blue-600">Privacy Statement</a>
+                    <span>.</span>
+                </div>
 
-            <div className="col-span-6 sm:flex sm:items-center sm:gap-4">
-                <button className="w-full rounded-md bg-blue-600 px-12 py-3 font-medium text-white 600 cursor-pointer">
-                Login
-                </button>
-            </div>
-        </form>
-      </div>
-    </main>
-</section>
+                <div className="col-span-6 sm:flex sm:items-center sm:gap-4">
+                    <button className="w-full rounded-md bg-blue-600 px-12 py-3 font-medium text-white 600 cursor-pointer">
+                    Login
+                    </button>
+                </div>
+            </form>
+        </div>
+        </main>
+    </section>
   )
 }
 
--- frontend/src/app/(auth)/auth/components/signupForm.tsx
+++ frontend/src/app/(auth)/auth/components/signupForm.tsx
@@ -5,10 +5,10 @@ import toast from 'react-hot-toast'
 import { useFormik } from 'formik'
 import { useState } from 'react'
 import axios from '@/lib/axios'
-import { Button } from '@/components/ui/button'
+import Image from 'next/image'
 import { signUpSchema } from '@/lib/validationSchema'
 
-const signupForm = () => {
+const SignupForm = () => {
     const [isLoading, setIsLoading] = useState<boolean>(false);
     const { values, errors, touched, handleChange, handleBlur, handleSubmit } = useFormik({
         initialValues: {
@@ -36,7 +36,76 @@ const signupForm = () => {
     })
 
     return (
-        <div className='h-screen flex items-center justify-center'>
+        <section className="bg-white lg:grid lg:min-h-screen lg:grid-cols-12">
+            <section className="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
+                <Image alt="" src="/gas-station.png" width={1000} height={1000} className="absolute inset-0 h-full w-full object-cover "/>
+                <div className="hidden lg:relative lg:block lg:p-12">
+                    <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
+                        Product Management System
+                    </h2>
+
+                    <p className="mt-4 leading-relaxed text-white/90">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem reprehenderit nemo qui excepturi, facilis aliquid enim suscipit necessitatibus ipsam cumque quia debitis illo nulla praesentium doloremque iusto accusantium in vero.
+                    </p>
+                </div>
+            </section>
+
+        <main className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6 w-full">
+            <div className="max-w-xl lg:max-w-3xl">
+                <div className="relative -mt-16 block lg:hidden">
+                    <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
+                        Product Management System
+                    </h1>
+                    <p className="mt-4 leading-relaxed text-gray-500">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum architecto repellat, aliquid laudantium illo quo praesentium ex quam vel exercitationem velit delectus quis id harum! Inventore praesentium voluptatem corporis fugiat!
+                    </p>
+                </div>
+
+                <form method='POST' onSubmit={handleSubmit} className="flex flex-col gap-4 w-md">
+
+                    <div className='flex flex-col gap-y-1 mb-5'>
+                        <p className='text-3xl font-semibold text-[#333]'>Sign Up</p>
+                        <p className='text-gray-500 text-sm'>Already have an account? <Link href="/auth/login" className='text-gray-800'>Sign In</Link></p>
+                    </div>
+
+                    <div className='flex flex-col gap-y-3'>
+                        <div>
+                            <input type="text" value={values.name} onChange={handleChange} onBlur={handleBlur}  placeholder='Full Name' name="name" className={`text-field ${errors.name && touched.name ? "border-2 border-danger focus:outline-none" : ""}`} />
+                            {errors.name && touched.name && <p className='error-text'>{errors.name}</p>}
+                        </div>
+                        <div>
+                            <input type="text" value={values.email} onChange={handleChange} onBlur={handleBlur}  placeholder='Email' name="email" className={`text-field ${errors.email && touched.email ? "border-2 border-danger focus:outline-none" : ""}`} />
+                            {errors.email && touched.email && <p className='error-text'>{errors.email}</p>}
+                        </div>
+                        <div>
+                            <input type="password" value={values.password} onChange={handleChange} onBlur={handleBlur} placeholder='Password' name="password" className={`text-field ${errors.password && touched.password ? "border-2 border-danger focus:outline-none" : ""}`}/>
+                            {errors.password && touched.password && <p className='error-text'>{errors.password}</p>}
+                        </div>
+                    </div>
+
+                    {/* <div className="col-span-6 text-sm">
+                        <span>By using this service, you understood and agree to the ClickCart Online Services </span>
+                        <Link href="#" className='text-blue-600'>Terms of Use</Link>
+                        <span className="mx-[5px]">and</span>
+                        <a href="#" className="text-blue-600">Privacy Statement</a>
+                        <span>.</span>
+                    </div> */}
+
+                    <div className="col-span-6 sm:flex sm:items-center sm:gap-4">
+                        <button className="w-full rounded-md bg-blue-600 px-12 py-3 font-medium text-white 600 cursor-pointer">
+                        Sign Up
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </main>
+    </section>
+    )
+}
+
+export default SignupForm
+
+        {/* <div className='h-screen flex items-center justify-center'>
             <div className='shadow-lg p-5 py-10 rounded-md'>
                 <div className='text-center'>
                     <p className='text-2xl font-semibold mb-3'>Signup</p>
@@ -59,8 +128,4 @@ const signupForm = () => {
 
                 </form>
             </div>
-        </div>
-    )
-}
-
-export default signupForm
+        </div> */}
